6d292ebbb005a52b1d107c19c6a823539481a3b9
dinhnam72 <dinhnam20162011@gmail.com>
2024-01-31 00:13:39 +0700

    prototype home

---
+++ html/startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+// Thiết lập thời gian chờ giữa các chuyển đổi (vd: 3 giây)
+setInterval(function() {
+    $('#carouselExample').carousel('next');
+}, 3000); // 3000 milliseconds = 3 giây
