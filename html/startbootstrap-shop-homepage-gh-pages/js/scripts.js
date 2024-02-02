b320b2e44afe58ec2a9e97557663b60468399ae8
dinhnam72 <dinhnam20162011@gmail.com>
2024-02-02 08:25:40 +0700

    prototype day4

--- html/startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ html/startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -7,5 +7,5 @@
 // Use this file to add JavaScript to your project
 // Thiết lập thời gian chờ giữa các chuyển đổi (vd: 3 giây)
 setInterval(function() {
-    $('#carouselExample').carousel('next');
-}, 3000); // 3000 milliseconds = 3 giây
+    $('#carouselExampleControls').carousel('next');
+}, 2000); // 3000 milliseconds = 3 giây
